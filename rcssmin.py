5396f5ec399b2ac57293657fb87ca888f3ecda8e
ndparker <ndparker@users.noreply.github.com>
2024-11-17 19:25:30 +0100

    Bump version to 1.2.0

--- rcssmin.py
+++ rcssmin.py
@@ -76,7 +76,7 @@ Supported python versions are 2.7 and 3.6+.
 """
 __author__ = u"Andr\xe9 Malo"
 __license__ = "Apache License, Version 2.0"
-__version__ = '1.1.3'
+__version__ = '1.2.0'
 __all__ = ['cssmin']
 
 import re as _re
